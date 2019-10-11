6cc5c4ab8ef316e35078d48cc590b2f514b1240c
Murat <muha188@mail.ru>
2019-10-11 16:04:03 +0600

    changes requst

--- src/main/java/com/exemple/sweater/domain/Message.java
+++ src/main/java/com/exemple/sweater/domain/Message.java
@@ -1,6 +1,9 @@
 package com.exemple.sweater.domain;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 
 @Entity
@@ -8,7 +11,10 @@ public class Message {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
+    @NotBlank(message = "Please fill the message")
+    @Length(max=2048, message = "Message too long")
     private String text;
+    @Length(max=255, message = "Message too long")
     private String tag;
 
     @ManyToOne(fetch = FetchType.EAGER)
